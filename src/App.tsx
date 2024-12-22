dab6cbce5be4eb5a226a396f31217c570060f6a9
atharvbhujannavar21 <83391812+atharvbhujannavar21@users.noreply.github.com>
2024-12-22 15:59:44 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div className="min-vh-100 bg-black">
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
